a632232497a9d52bd92838aaac60d1a340c7e95b
syamal16 <jayasree11.k@gmail.com>
2017-02-13 13:55:00 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String args[])
     {
-        System.out.println("Hello world for Jenkins!");
+        System.out.println("Jenkins Demo!");
     }
 }
